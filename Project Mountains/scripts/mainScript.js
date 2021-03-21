26770b29c9820e156ab3f8f278d747a8d1377f9f
AxyseCom <axyse.com@gmail.com>
2021-03-21 17:36:11 +0300

    added files

--- Project Mountains/scripts/mainScript.js
+++ Project Mountains/scripts/mainScript.js
@@ -1,1 +1,33 @@
-alert("hello ")
+
+$(".nav-link").click(function () {
+  var div_id = $(this).attr("id"); // gives you the ID of the clicked div
+  if (div_id === "id1") {
+    $("html, body").animate(
+      {
+        scrollTop: $(".about-block").offset().top, // класс объекта к которому приезжаем
+      },
+      1000
+    );
+  } else if (div_id === "id2") {
+    $("html, body").animate(
+      {
+        scrollTop: $(".our-articles-block").offset().top, // класс объекта к которому приезжаем
+      },
+      1000
+    );
+  } else if (div_id === "id3") {
+    $("html, body").animate(
+      {
+        scrollTop: $(".partners-block").offset().top, // класс объекта к которому приезжаем
+      },
+      1000
+    );
+  } else if (div_id === "id4") {
+    $("html, body").animate(
+      {
+        scrollTop: $(".mail-block").offset().top, // класс объекта к которому приезжаем
+      },
+      1000
+    );
+  }
+});
